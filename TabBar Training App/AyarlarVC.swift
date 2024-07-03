32a7d10c6e000fbf4f8890791ac7044e15d4a08e
emirhangecer <emirhan19072121@gmail.com>
2024-07-03 18:02:13 +0300

    first working

---
+++ TabBar Training App/AyarlarVC.swift
@@ -0,0 +1,19 @@
+//
+//  AyarlarVC.swift
+//  TabBar Training App
+//
+//  Created by Emirhan on 3.07.2024.
+//
+
+import UIKit
+
+class AyarlarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+}
